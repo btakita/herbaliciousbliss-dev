c5841c3b0cf44eda3fb55c1e32680cee4ea026fb
Brian Takita <brian.takita@gmail.com>
2024-03-13 01:12:47 -0400

    -herbaliciousbliss-dev-bin: tmux-dev:
    	- tmux rename-window dev
    	top-right: bun -b run dev: cd outside of send-keys

--- app/-herbaliciousbliss-dev-bin/bin/tmux-dev.sh
+++ app/-herbaliciousbliss-dev-bin/bin/tmux-dev.sh
@@ -3,12 +3,12 @@ DIR_DEFAULT=$(dirname $0)/../..
 DIR="${DIR:-$DIR_DEFAULT}"
 
 cd $DIR
-tmux rename-window dev
 tmux split-window -v $SHELL
 tmux send-keys 'tig' 'C-m'
 tmux split-window -h $SHELL
 tmux select-pane -t 1
 tmux split-window -h $SHELL
-tmux send-keys "cd $DIR/app/herbaliciousbliss-site; bun -b run dev" 'C-m'
+cd $DIR/app/herbaliciousbliss-site
+tmux send-keys "bun -b run dev" 'C-m'
 tmux select-pane -t 1
 tmux rename-window herbaliciousbliss
